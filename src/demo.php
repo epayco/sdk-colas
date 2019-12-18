ea0bf570a733d675f7588079bc2604495d36d7b7
DANIEL QUIROZ <danykyroz@gmail.com>
2019-12-18 10:52:59 -0500

    Eliminar llaves del codigo

--- src/demo.php
+++ src/demo.php
@@ -3,8 +3,8 @@ include ('src/Colas.php');
 
 use Epayco\Colas\Colas;
 
-$public_key="70efb5772437cc7afbe15b77fe8dea";
-$private_key="ef98758224a02e086f74beeaf5bc171f";
+$public_key="PUBLIC_KEY_EPAYCO";
+$private_key="PRIVATE_KEY_EPAYCO";
 $colas=new Colas($public_key,$private_key);
 $addCola=$colas->addMessage('movimientos','abonar_transaccion','1234');	
 var_dump($addCola);
